df1fc7df6bb17feec7d798a6f46f9c96098c93b9
kei <kei.takakuda.0415@icloud.com>
2018-07-08 18:38:45 +0900

    better FizzBuzz

--- hello.go
+++ hello.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	n := 3
-	switch n {
-	case 3:
-		n = n - 1
-		fallthrough
-	case 2:
-		n = n - 1
-		fallthrough
-	case 1:
-		n = n - 1
-		fmt.Println(n)
+	n := 0
+	for {
+		n++
+		if n > 15 {
+			break
+		}
+		switch {
+		case n%15 == 0:
+			fmt.Println("FizzBuzz")
+		case n%5 == 0:
+			fmt.Println("Fizz")
+		case n%3 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(n)
+		}
 	}
 }
