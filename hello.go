150344ac1e2b5ef091c9ff766fc1b7e94746f833
kei <kei.takakuda.0415@icloud.com>
2018-07-08 19:49:14 +0900

    func

--- hello.go
+++ hello.go
@@ -2,22 +2,10 @@ package main
 
 import "fmt"
 
+func hello() {
+	fmt.Println("hello")
+}
+
 func main() {
-	n := 0
-	for {
-		n++
-		if n > 15 {
-			break
-		}
-		switch {
-		case n%15 == 0:
-			fmt.Println("FizzBuzz")
-		case n%5 == 0:
-			fmt.Println("Fizz")
-		case n%3 == 0:
-			fmt.Println("Buzz")
-		default:
-			fmt.Println(n)
-		}
-	}
+	hello()
 }
