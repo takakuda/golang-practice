c355ba9547bc402a134d5cd482bb4f3abf5a3d0e
kei <kei.takakuda.0415@icloud.com>
2018-07-09 00:11:41 +0900

    errors package

--- hello.go
+++ hello.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+func div(i, j int) (int, error) {
+	if j == 0 {
+		// 自作のエラーを返す
+		return 0, errors.New("divied by zero")
+	}
+	return i / j, nil
+}
 
 func main() {
-	file, err := os.Open("hello.go")
+	n, err := div(10, 0)
 	if err != nil {
-		// エラー処置
-		// returnなどで処理を別の場所に抜ける
+		// エラーを出力しプログラムを終了する
+		log.Fatal(err)
 	}
-	// fileを用いた処理
+	fmt.Println(n)
 }
