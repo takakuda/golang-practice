aae1693e7235652536a7174c31bd73d5c6b41101
kei <kei.takakuda.0415@icloud.com>
2018-07-08 17:54:42 +0900

    break, continue

--- hello.go
+++ hello.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	n := 0
+	for {
+		n++
+		if n > 10 {
+			break //ループを抜ける
+		}
+		if n%2 == 0 {
+			continue // 偶数なら次の繰り返しへ
+		}
+		fmt.Println(n) //奇数のみ表示
 	}
 }
