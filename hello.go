58cfd1c03843ebbb09236d3f73de5235ebc61c18
kei <kei.takakuda.0415@icloud.com>
2018-07-08 17:48:00 +0900

    for文

--- hello.go
+++ hello.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a, b := 10, 100
-	if a > b {
-		fmt.Println("a is lager than b")
-	} else if a < b {
-		fmt.Println("b is lager than a")
-	} else {
-		fmt.Println("a equal b")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 	}
 }
