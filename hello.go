825ff91ac4845d3b9d7326613722469b75fd1921
kei <kei.takakuda.0415@icloud.com>
2018-07-09 09:24:44 +0900

    関数リテラル

--- hello.go
+++ hello.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
 )
 
-func div(i, j int) (result int, err error) {
-	if j == 0 {
-		err = errors.New("devied by zero")
-		return // return 0, errと同じ
-	}
-	result = i / j
-	return
-}
-
 func main() {
-	n, err := div(10, 0)
-	if err != nil {
-		// エラーを出力しプログラムを終了する
-		log.Fatal(err)
-	}
-	fmt.Println(n)
+	func(i, j int) {
+		fmt.Println(i + j)
+	}(2, 4)
 }
