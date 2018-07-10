b59ca69922b184adf71a2bc499f432b20e8c9268
kei <kei.takakuda.0415@icloud.com>
2018-07-10 09:18:57 +0900

    配列　引数

--- hello.go
+++ hello.go
@@ -4,5 +4,14 @@ import (
 	"fmt"
 )
 
-arr := [4]string{"a", "b", "c", "d"}
-arr := [...]string{"a", "b", "c", "d"}
+func fn(arr1 [4]string) {
+	fmt.Println(arr)
+}
+
+func main() {
+  var arr1 [4]string{"a", "b", "c", "d"}
+	var arr1 [4]string
+	// var arr2 [5]string
+
+	fn(arr1)
+}
