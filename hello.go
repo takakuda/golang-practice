a1c2b6c51591fd6f697b684e986df772464ba309
kei <kei.takakuda.0415@icloud.com>
2018-07-10 22:56:49 +0900

    スライス

--- hello.go
+++ hello.go
@@ -5,9 +5,8 @@ import (
 )
 
 func main() {
-	var s []string
-	s = append(s, "a")
-	s = append(s, "b")
-	s = append(s, "c", "d")
-	fmt.Println(s)
+	s1 := []string{"a", "b"}
+	s2 := []string{"c", "d"}
+	s1 = append(s1, s2)
+	fmt.Println(s1)
 }
