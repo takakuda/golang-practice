4ee074b64035a0a7bcae75c995e2953af1924737
kei <kei.takakuda.0415@icloud.com>
2018-07-12 01:33:50 +0900

    type

---
+++ type.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type ID int
+type Priority int
+
+func ProcessTask(id ID priority Priority) {
+}
+
+func main() {
+  var id := 3
+  var priority := 5
+  ProcessTask(id, priority)
+}
