fa5a98cd6a2eb41eaff0d7417d57cd9da85cdaa1
kei <kei.takakuda.0415@icloud.com>
2018-07-11 23:10:38 +0900

    panic

--- pointa.go
+++ pointa.go
@@ -1,19 +1,16 @@
 package main
 
-func callByValue(i int) {
-	i = 20
-}
-
-func callByRef(i *int) {
-	*i = 20
-}
+import "fmt"
+import "log"
 
 func main() {
-	file, err := os.Open("./error.go")
-	if err != nil {
-		// エラー処理
-	}
-	// 関数を抜ける前に必ず実行される
-	defer file.Close()
-	// 正常処理
+	defer func() {
+		err := recover()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	a := []int{1, 2, 3}
+	fmt.Println(a[10])
 }
