1813e1c4ebbc10f6435d64e4946047c10b1f4cd3
kei <kei.takakuda.0415@icloud.com>
2018-07-13 00:41:53 +0900

    ファイルへの書き込み

--- os.go
+++ os.go
@@ -5,12 +5,26 @@ import (
 	"os"
 )
 
+type Writer interface {
+	Write(p []byte) (n int, err error)
+}
+
 func main() {
 	// ファイルを生成
 	file, err := os.Create("./file.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// プログラムが終わったらファイルを閉じる
 	defer file.Close()
+
+	// 書き込むデータを[]byteで用意する
+	message := []byte("hello world\n")
+
+	// Writeを用いて書き込む
+	_, err = file.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
