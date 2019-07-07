c811464dca6887a7dde24fdcb247352178cfab80
kei <kei.takakuda.0415@icloud.com>
2019-07-07 21:01:51 +0900

    type inference

--- zoo/main.go
+++ zoo/main.go
@@ -13,7 +13,6 @@ func main() {
   fmt.Println(animals.ElephantFeed())
   fmt.Println(animals.MonkeyFeed())
   fmt.Println(animals.RabbitFeed())
-  */
   fmt.Println("My", "name", "is", "golang")
   fmt.Printf("数値=%d\n", 5)
   fmt.Printf("10進法=%d 2進法=%b 8進法=%o 16進法=%x\n", 17, 17, 17, 17)
@@ -21,4 +20,11 @@ func main() {
   fmt.Printf("数値=%v 文字列=%v 配列=%v\n", 5, "Golang", [...]int{1, 2, 3})
   fmt.Printf("数値=%#v 文字列=%#v 配列=%#v\n", 5, "Golang", [...]int{1, 2, 3})
   fmt.Printf("数値=%T 文字列=%T 配列=%T\n", 5, "Golang", [...]int{1, 2, 3})
+  */
+  n := one()
+  fmt.Println(n)
+}
+
+func one() int {
+  return 1
 }
