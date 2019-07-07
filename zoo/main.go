4231c0b68bbdf85144f2e6e45ac4f896b9eca748
kei <kei.takakuda.0415@icloud.com>
2019-07-07 20:49:07 +0900

    fmt.Printf

--- zoo/main.go
+++ zoo/main.go
@@ -18,4 +18,7 @@ func main() {
   fmt.Printf("数値=%d\n", 5)
   fmt.Printf("10進法=%d 2進法=%b 8進法=%o 16進法=%x\n", 17, 17, 17, 17)
   fmt.Printf("%d年%d月%d日\n", 2015, 12, 1)
+  fmt.Printf("数値=%v 文字列=%v 配列=%v\n", 5, "Golang", [...]int{1, 2, 3})
+  fmt.Printf("数値=%#v 文字列=%#v 配列=%#v\n", 5, "Golang", [...]int{1, 2, 3})
+  fmt.Printf("数値=%T 文字列=%T 配列=%T\n", 5, "Golang", [...]int{1, 2, 3})
 }
