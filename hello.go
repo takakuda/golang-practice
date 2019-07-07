838d9afb6b5dbbcdc1698d8bc32a746c820e7c4d
kei <kei.takakuda.0415@icloud.com>
2019-07-07 18:08:38 +0900

    hello go

--- hello.go
+++ hello.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
 )
 
 func main() {
-	s1 := []string{"a", "b"}
-	s2 := []string{"c", "d"}
-	s1 = append(s1, s2)
-	fmt.Println(s1)
+  fmt.Println("Hello, World!")
 }
