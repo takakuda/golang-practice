ac2f93bf7ddaaef716126e91eefc455204bc95b1
kei <kei.takakuda.0415@icloud.com>
2019-07-07 20:42:17 +0900

    fmt.Printf

--- zoo/main.go
+++ zoo/main.go
@@ -3,11 +3,19 @@ package main
 import (
   "fmt"
 
-  "./animals"
+  _ "./animals"
 )
 
 func main() {
+  /*
+  fmt.Println(AppName())
+
   fmt.Println(animals.ElephantFeed())
   fmt.Println(animals.MonkeyFeed())
   fmt.Println(animals.RabbitFeed())
+  */
+  fmt.Println("My", "name", "is", "golang")
+  fmt.Printf("数値=%d\n", 5)
+  fmt.Printf("10進法=%d 2進法=%b 8進法=%o 16進法=%x\n", 17, 17, 17, 17)
+  fmt.Printf("%d年%d月%d日\n", 2015, 12, 1)
 }
