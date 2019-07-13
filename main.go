0bacba1a939186e9f1082fb47709bbd8e9f4d068
kei <kei.takakuda.0415@icloud.com>
2019-07-13 21:33:50 +0900

    switch simple sentence

--- main.go
+++ main.go
@@ -5,19 +5,10 @@ import (
 )
 
 func main() {
-  s := "A"
-  switch s {
-  case "A":
-    s += "B"
-    fallthrough
-  case "B":
-    s += "C"
-    fallthrough
-  case "C":
-    s += "D"
-    fallthrough
-  default:
-    s += "E"
+  switch n := 2; n {
+  case 1, 3, 5, 7, 9:
+    fmt.Printf("%d is odd\n", n)
+  case 2, 4, 6, 8, 10:
+    fmt.Printf("%d is even\n", n)
   }
-  fmt.Println(s)
 }
