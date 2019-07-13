384411a4deb8b728180b27e5fa712798fce38bd5
kei <kei.takakuda.0415@icloud.com>
2019-07-13 21:43:42 +0900

    type assertion

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 )
 
 func main() {
-  switch n := 2; n {
-  case 1, 3, 5, 7, 9:
-    fmt.Printf("%d is odd\n", n)
-  case 2, 4, 6, 8, 10:
-    fmt.Printf("%d is even\n", n)
+  var x interface{} = 1
+  if x == nil {
+    fmt.Println("x is nil")
+  } else if i, isInt := x.(int); isInt {
+    fmt.Println("x is integer : %d\n", i)
+  } else if s, isString := x.(string); isString {
+    fmt.Println(s)
+  } else {
+    fmt.Println("unsupported type!")
   }
 }
