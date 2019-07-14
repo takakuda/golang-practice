efda0b4eefabb8d0b8c7c06eaf73f032ddb0d4b8
kei <kei.takakuda.0415@icloud.com>
2019-07-15 03:33:09 +0900

    init

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
   "fmt"
-  "runtime"
 )
 
+func init() {
+  fmt.Println("init()")
+}
 
 func main() {
-  go fmt.Println("Yeah!")
-  fmt.Printf("NumCPU: %d\n", runtime.NumCPU())
-  fmt.Printf("NumGoroutine: %d\n", runtime.NumGoroutine())
-  fmt.Printf("Version: %s\n", runtime.Version())
+  fmt.Println("main()")
 }
