35943f7f46a66289338e9001fa28a9c4446e5973
kei <kei.takakuda.0415@icloud.com>
2019-07-15 03:30:48 +0900

    goroutine

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
   "fmt"
+  "runtime"
 )
 
-func sub() {
-  for {
-    fmt.Println("sub loop")
-  }
-}
 
 func main() {
-  go sub()
-  for {
-    fmt.Println("main loop")
-  }
+  go fmt.Println("Yeah!")
+  fmt.Printf("NumCPU: %d\n", runtime.NumCPU())
+  fmt.Printf("NumGoroutine: %d\n", runtime.NumGoroutine())
+  fmt.Printf("Version: %s\n", runtime.Version())
 }
