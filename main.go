1a4a4645020fbe2dc5cb3aed605c2d9fe97cb91e
kei <kei.takakuda.0415@icloud.com>
2019-07-15 20:30:21 +0900

    s ...int

--- main.go
+++ main.go
@@ -4,18 +4,14 @@ import (
   "fmt"
 )
 
+func sum(s ...int) int {
+  n := 0
+  for _, v := range s {
+    n += v
+  }
+  return n
+}
+
 func main() {
-  a := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-
-  s1 := a[2:4]
-  fmt.Println(len(s1))
-  fmt.Println(cap(s1))
-
-  s2 := a[2:4:4]
-  fmt.Println(len(s2))
-  fmt.Println(cap(s2))
-
-  s3 := a[2:4:6]
-  fmt.Println(len(s3))
-  fmt.Println(cap(s3))
+  fmt.Println(sum(1, 2, 3))
 }
