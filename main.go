9039b2943e68c45b69bfd776ff5b79a1372c14fc
kei <kei.takakuda.0415@icloud.com>
2019-07-15 20:20:49 +0900

    full slice expressions

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 )
 
 func main() {
-  s1 := []int{1, 2, 3, 4, 5}
-  s2 := []int{10, 11}
-  n := copy(s1, s2)
-  fmt.Println(n)
-  fmt.Println(s1)
+  a := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+  s1 := a[2:4]
+  fmt.Println(len(s1))
+  fmt.Println(cap(s1))
+
+  s2 := a[2:4:4]
+  fmt.Println(len(s2))
+  fmt.Println(cap(s2))
+
+  s3 := a[2:4:6]
+  fmt.Println(len(s3))
+  fmt.Println(cap(s3))
 }
