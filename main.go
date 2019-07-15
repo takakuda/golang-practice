f6178c1dd7e1459979b505640877d3d81d421d60
kei <kei.takakuda.0415@icloud.com>
2019-07-15 19:12:41 +0900

    make

--- main.go
+++ main.go
@@ -4,20 +4,7 @@ import (
   "fmt"
 )
 
-var S = ""
-
-func init() {
-  S = S + "A"
-}
-
-func init() {
-  S = S + "B"
-}
-
-func init() {
-  S = S + "C"
-}
-
 func main() {
-  fmt.Println(S)
+  s := make([]int, 10)
+  fmt.Println(s)
 }
