49fa6a95573aee5a39019ee4f0d8777d3fb83883
kei <kei.takakuda.0415@icloud.com>
2019-08-04 19:16:08 +0900

    map

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
-func pow(a []int) {
-  for i, v := range a {
-    a[i] = v * v
-  }
-  return
-}
-
 func main() {
-  a := []int{1, 2, 3}
-  pow(a)
-  fmt.Println(a)
+	m := make(map[int]string)
+	m[1] = "US"
+	fmt.Println(m)
 }
