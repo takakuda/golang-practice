fe77fc8667760a3eec29341146229d8cbf1daf43
kei <kei.takakuda.0415@icloud.com>
2019-08-04 20:11:20 +0900

    channel close

--- main.go
+++ main.go
@@ -4,20 +4,15 @@ import (
 	"fmt"
 )
 
-func receiver(ch <-chan int) {
-	for {
-		i := <-ch
-		fmt.Println(i)
-	}
-}
-
 func main() {
-	ch := make(chan int)
-
-	go receiver(ch)
-	i := 0
-	for i < 1000 {
-		ch <- i
-		i++
-	}
+	ch := make(chan int, 3)
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	close(ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
