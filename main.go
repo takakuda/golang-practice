63c121428d59318909977180f42eecc1caa289cf
kei <kei.takakuda.0415@icloud.com>
2019-08-04 20:06:09 +0900

    goroutine channel

--- main.go
+++ main.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 )
 
+func receiver(ch <-chan int) {
+	for {
+		i := <-ch
+		fmt.Println(i)
+	}
+}
+
 func main() {
-	ch := make(chan int, 10)
-	ch <- 5
-	i := <-ch
-	fmt.Println(i)
+	ch := make(chan int)
+
+	go receiver(ch)
+	i := 0
+	for i < 1000 {
+		ch <- i
+		i++
+	}
 }
