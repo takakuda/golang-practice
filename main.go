875bb673beedab8f67a7250f262658ea75687a2a
kei <kei.takakuda.0415@icloud.com>
2019-08-12 21:44:53 +0900

    pointer

--- main.go
+++ main.go
@@ -2,33 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func receive(name string, ch <-chan int) {
-	for {
-		i, ok := <-ch
-		if ok == false {
-			break
-		}
-		fmt.Println(name, i)
-	}
-	fmt.Println(name + " is done.")
-}
-
 func main() {
-	ch := make(chan int, 20)
-
-	go receive("1st goroutine", ch)
-	go receive("2nd goroutine", ch)
-	go receive("3rd goroutine", ch)
-
-	i := 0
-	for i < 100 {
-		ch <- i
-		i++
-	}
-	close(ch)
-
-	time.Sleep(3 * time.Second)
+	var p *int
+	fmt.Println(p == nil)
 }
